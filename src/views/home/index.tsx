c28937c61e8ce343c6a4f2f0a08092d63dc7b178
Zack Traczyk <me@zacktraczyk.com>
2023-12-27 14:22:28 -0600

    add Speaker Carousel

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/Navbar"
 import Landing from "./1 - Landing"
 import HeroBlurb from "./2 - Hero Blurb"
 import Milestones from "./4 - Milestones"
-// import Speakers from "./5 - Speakers"
+import Speakers from "./5 - Speakers"
 import QA from "./6 - Q&A"
 import Sponsors from "./7 - Sponsors"
 import Footer from "../../components/Footer"
@@ -38,7 +38,7 @@ const Home = () => {
         <HeroBlurb id='about' />
         <PrizeTracks id='prize-tracks' />
         <Milestones id='milestones' />
-        {/* <Speakers id='speakers' /> */}
+        <Speakers id='speakers' />
         <QA id='qa' />
         <Sponsors id='sponsors' />
         <div className='h-40' />
