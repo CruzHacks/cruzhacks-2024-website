7275ca5dc0e05ce982c2010bed380064044b9082
Zack Traczyk <me@zacktraczyk.com>
2024-01-16 14:11:13 -0800

    update styling

--- src/components/teams/TeamDisplay.tsx
+++ src/components/teams/TeamDisplay.tsx
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from "react"
-import { TeamFormationProps, TeamMemberProps, TeamDisplayProps, TeamMemberTagProps } from "../../utils/types"
+import {
+  TeamFormationProps,
+  TeamMemberProps,
+  TeamDisplayProps,
+  TeamMemberTagProps,
+} from "../../utils/types"
 import useAuth from "../../hooks/useAuth"
-import { deleteTeam, lockTeam, removeTeamMember } from "../../utils/apis/firebase"
+import {
+  deleteTeam,
+  lockTeam,
+  removeTeamMember,
+} from "../../utils/apis/firebase"
 import Modal from "../Modal"
 import { LockClosedIcon, TrashIcon } from "@heroicons/react/24/solid"
 import toast from "react-hot-toast"
-
+import { classNames } from "../../utils/string"
 
 export const TeamDisplay = (props: TeamDisplayProps) => {
-    const {
-        auth: { user },
-      } = useAuth()
-      
+  const {
+    auth: { user },
+  } = useAuth()
+
   const [openConfirm, setOpenConfirm] = useState<boolean>(false)
   const [openDelete, setOpenDelete] = useState<boolean>(false)
 
-
   if (!user) throw new Error("User could not be fetched from session")
   return (
-    <div>
+    <>
       <Modal
         open={openConfirm}
         setOpen={setOpenConfirm}
@@ -27,15 +35,14 @@ export const TeamDisplay = (props: TeamDisplayProps) => {
         actionText='LOCK IN'
         description='Are you sure you want to lock in your team? You will no longer be able to add or remove members.'
         actionFunc={() => {
-                lockTeam(
-                    user,
-                    props.teamPage.teamName,
-                ).then((newTeamPage: TeamFormationProps) => {
-                    props.setTeamPage(newTeamPage)
-                    toast.success("Team Locked In")
-                }).catch((error) => {
-                    toast.error(error.message)
-                })
+          lockTeam(user, props.teamPage.teamName)
+            .then((newTeamPage: TeamFormationProps) => {
+              props.setTeamPage(newTeamPage)
+              toast.success("Team Locked In")
+            })
+            .catch(error => {
+              toast.error(error.message)
+            })
         }}
       />
       <Modal
@@ -46,113 +53,117 @@ export const TeamDisplay = (props: TeamDisplayProps) => {
         actionText='DELETE TEAM'
         description='Are you sure you want to delete your team? This action cannot be undone.'
         actionFunc={() => {
-            deleteTeam(
-                user,
-                props.teamPage.teamName,
-            ).then((newTeamPage: TeamFormationProps) => {
-                props.setTeamPage(newTeamPage)
-                props.setTeamStatus("JOIN")
-                toast.success("Team Deleted")
-            }).catch((error) => {
-                toast.error(error.message)
+          deleteTeam(user, props.teamPage.teamName)
+            .then((newTeamPage: TeamFormationProps) => {
+              props.setTeamPage(newTeamPage)
+              props.setTeamStatus("JOIN")
+              toast.success("Team Deleted")
+            })
+            .catch(error => {
+              toast.error(error.message)
             })
         }}
       />
-    <div className='flex flex-col flex-wrap place-content-center py-3'>
-        <div className='text-center text-lg text-[#192F6F]'>
+      <div className='space-y-10'>
+        <div className='text-center font-title text-lg'>
           {props.teamPage.teamName || "<- No Team ->"}
         </div>
-    </div>
-      <div className='flex flex-col gap-2 px-5 pb-3'>
-        {props.teamPage.teamMembers.map((member: TeamMemberProps) => {
-          return (
-            <TeamMemberTag
-              key={member.memberEmail}
-              email={member.memberEmail}
-              name={member.memberName}
-              type='ACCEPTED'
-              teamLeader={props.teamPage.teamLeader}
-              setTeamPage={props.setTeamPage}
-            />
-          )
-        })}
-        {props.teamPage.invitedTeamMembers.map((member: TeamMemberProps) => {
-          return (
-            <TeamMemberTag
-              key={member.memberEmail}
-              email={member.memberEmail}
-              name={member.memberName}
-              type='INVITED'
-              teamLeader={props.teamPage.teamLeader}
-              setTeamPage={props.setTeamPage}
-            />
-          )
-        })}
-      </div>
-      {props.teamPage.teamLeader === user?.email ? (
-        <div className='flex w-full flex-row flex-wrap place-content-center gap-5 py-5'>
-          <button
-          className='rounded-md border-2 border-[#F81A16] bg-[#F81A16] px-1.5 py-0.5 text-sm text-[#FFF] hover:bg-[#FFF] hover:text-[#F81A16]'
-            onClick={() => setOpenDelete(true)}
-          >
-            DELETE TEAM
-          </button>
-          <div>
-            {!props.teamPage.lockedIn ? (
-                <button
-                className='rounded-md border-2 border-[#10E926] bg-[#10E926] px-1.5 py-0.5 text-sm text-[#FFF] hover:bg-[#FFF] hover:text-[#10E926]'
-                onClick={() => setOpenConfirm(true)}
-                >
-                LOCK IN TEAM
-                </button>
-            ) : null}
+        <div className='space-y-3'>
+          <div className='text-left font-title'>Team Members</div>
+          <div className='flex flex-col gap-3'>
+            {props.teamPage.teamMembers.map((member: TeamMemberProps) => {
+              return (
+                <TeamMemberTag
+                  key={member.memberEmail}
+                  email={member.memberEmail}
+                  name={member.memberName}
+                  type='ACCEPTED'
+                  teamLeader={props.teamPage.teamLeader}
+                  setTeamPage={props.setTeamPage}
+                />
+              )
+            })}
+            {props.teamPage.invitedTeamMembers.map(
+              (member: TeamMemberProps) => {
+                return (
+                  <TeamMemberTag
+                    key={member.memberEmail}
+                    email={member.memberEmail}
+                    name={member.memberName}
+                    type='INVITED'
+                    teamLeader={props.teamPage.teamLeader}
+                    setTeamPage={props.setTeamPage}
+                  />
+                )
+              }
+            )}
+          </div>
         </div>
+        {props.teamPage.teamLeader === user?.email && (
+          <div className='flex w-full flex-row flex-wrap place-content-center gap-5'>
+            <button
+              className='w-full rounded-md bg-error px-5 py-2 text-sm font-bold text-[#FFF] hover:bg-error/70 md:w-fit'
+              onClick={() => setOpenDelete(true)}
+            >
+              DELETE TEAM
+            </button>
+            {!props.teamPage.lockedIn && (
+              <button
+                className='w-full rounded-md bg-success px-5 py-2 text-sm font-bold text-[#FFF] hover:bg-success/70 md:w-fit'
+                onClick={() => setOpenConfirm(true)}
+              >
+                LOCK IN TEAM
+              </button>
+            )}
+          </div>
+        )}
       </div>
-      ) : null}
-    </div>
+    </>
   )
 }
 
 const TeamMemberTag = (props: TeamMemberTagProps) => {
-    const {
-        auth: { user },
-    } = useAuth()
+  const {
+    auth: { user },
+  } = useAuth()
 
-    const [type, setType] = useState<string>("")
-    
-    useEffect(() => {
-        if (props.teamLeader === props.email)
-            setType("LEADER")
-        else if (props.type === "ACCEPTED")
-            setType("MEMBER")
-        else if (props.type === "INVITED")
-            setType("INVITED")
-    },[props.setTeamPage])
+  const [type, setType] = useState<string>("")
+
+  useEffect(() => {
+    if (props.teamLeader === props.email) setType("LEADER")
+    else if (props.type === "ACCEPTED") setType("MEMBER")
+    else if (props.type === "INVITED") setType("INVITED")
+  }, [props.setTeamPage])
 
   return (
-    <div className='flex w-full flex-row justify-between rounded-md border-2 border-[#C2C6C4] p-2'>
-        <div className='text-md text-center text-[#192F6F]'>
+    <div className='flex w-full flex-row items-center justify-between rounded-md bg-blue-imperial p-2 md:px-5'>
+      <p
+        className={classNames(
+          user?.email === props.email ? "text-orange" : "text-white",
+          "text-md truncate text-center font-subtext"
+        )}
+      >
         {props.name || "<Empty>"}
-      </div>
-      <div className='flex flex-row flex-wrap place-content-center gap-1'>
-      {props.teamLeader === user?.email && props.email !== props.teamLeader  ? (
-        <button
-        className='rounded-md border-2 border-[#F81A16] px-1.5 py-0.5 text-sm text-[#F81A16] hover:bg-[#F81A16] hover:text-[#FFF]'
-        onClick={() => {
-            removeTeamMember(
-            user,
-            props.email,
-            ).then((newTeamPage: TeamFormationProps) => {
-                props.setTeamPage(newTeamPage)
-                toast.success("Team Member Removed")
-            }).catch((error) => {
-                toast.error(error.message)
-            })
-          }}
-        >
-          Remove
-        </button>
-      ) : null}
+      </p>
+      <div className='flex flex-row flex-wrap place-content-center gap-1 md:gap-3'>
+        {props.teamLeader === user?.email &&
+          props.email !== props.teamLeader && (
+            <button
+              className='rounded-md border-2 border-[#F81A16] px-1.5 py-0.5 text-sm font-bold text-[#F81A16] hover:bg-[#F81A16] hover:text-[#FFF]'
+              onClick={() => {
+                removeTeamMember(user, props.email)
+                  .then((newTeamPage: TeamFormationProps) => {
+                    props.setTeamPage(newTeamPage)
+                    toast.success("Team Member Removed")
+                  })
+                  .catch(error => {
+                    toast.error(error.message)
+                  })
+              }}
+            >
+              Remove
+            </button>
+          )}
         <TeamMemberBadge type={type}></TeamMemberBadge>
       </div>
     </div>
@@ -160,30 +171,27 @@ const TeamMemberTag = (props: TeamMemberTagProps) => {
 }
 
 export interface BadgeProps {
-    type: string
+  type: string
 }
 
-
 const TeamMemberBadge = (props: BadgeProps) => {
-    if (props.type === 'LEADER')
-        return (
-            <div className='rounded-md border-2 border-[#ECCC2B] bg-[#ECCC2B] px-1.5 py-0.5 text-sm text-[#FFF]'>
-                LEADER
-            </div>
-        )
-    else if (props.type === 'MEMBER')
-        return (
-            <div className='rounded-md border-2 border-[#139A43] bg-[#139A43] px-1.5 py-0.5 text-sm text-[#FFF]'>
-                MEMBER
-            </div>
-        )
-    else if (props.type === 'INVITED')
-        return (
-            <div className='rounded-md border-2 border-[#FA4437] bg-[#FA4437] px-1.5 py-0.5 text-sm text-[#FFF]'>
-                INVITED
-            </div>
-        )
+  if (props.type === "LEADER")
     return (
-      <></>
+      <div className='w-24 rounded-md border-2 border-pink bg-pink px-1.5 py-0.5 text-center text-sm font-bold text-[#FFF]'>
+        LEADER
+      </div>
     )
+  else if (props.type === "MEMBER")
+    return (
+      <div className='w-24 rounded-md border-2 border-blue-button bg-blue-button px-1.5 py-0.5 text-center text-sm font-bold text-[#FFF]'>
+        MEMBER
+      </div>
+    )
+  else if (props.type === "INVITED")
+    return (
+      <div className='w-24 rounded-md border-2 border-[#FA4437] bg-[#FA4437] px-1.5 py-0.5 text-center text-sm font-bold text-[#FFF]'>
+        INVITED
+      </div>
+    )
+  return <></>
 }
--- src/components/teams/TeamInvite.tsx
+++ src/components/teams/TeamInvite.tsx
@@ -3,48 +3,47 @@ import useAuth from "../../hooks/useAuth"
 import { inviteTeamMember } from "../../utils/apis/firebase"
 import { TeamInviteProps } from "../../utils/types"
 import toast from "react-hot-toast"
+import TextInput from "../inputs/TextInput"
+import { EnvelopeIcon } from "@heroicons/react/24/outline"
 
 export const TeamInvite = (props: TeamInviteProps) => {
-    const {
-        auth: { user },
-    } = useAuth()
+  const {
+    auth: { user },
+  } = useAuth()
 
-    if (!user) throw new Error("User could not be fetched from session")
+  if (!user) throw new Error("User could not be fetched from session")
 
-    const [invitedMemberEmail, setInvitedMemberEmail] = useState<string>("")
+  const [invitedMemberEmail, setInvitedMemberEmail] = useState<string>("")
 
-    return (
-    <div className='flex w-full flex-col flex-wrap place-content-center gap-4 py-3'>
-        <div className='text-center text-lg text-[#192F6F]'>
-            Invite Members
-        </div>
-          <input
-            type='text'
-            placeholder='Enter Email'
-            className='w-3/4 rounded-xl text-[#000]'
-            value={invitedMemberEmail}
-            onChange={e => setInvitedMemberEmail(e.target.value)}
-          />
-          <div className="flex flex-wrap place-content-center">
-            <button
-                className='rounded-md border-2 border-blue-imperial bg-[#FFF] px-1.5 py-0.5 text-center text-sm text-blue-imperial hover:bg-blue-imperial hover:text-[#FFF]'
-                onClick={() => {
-
-                        inviteTeamMember(
-                            user,
-                            invitedMemberEmail,
-                            ).then((team) => {
-                                props.setTeamPage(team)
-                                setInvitedMemberEmail("")
-                                toast.success("Team Member Invited!")
-                            }).catch((error) => {
-                                toast.error(error.message)
-                            })
-                }}
-            >
-            INVITE MEMBER
-            </button>
-        </div>
-      </div>
-    )
+  return (
+    <div className='flex w-full flex-col flex-wrap place-content-center gap-4 p-10'>
+      <div className='text-center font-title text-lg'>Invite Members</div>
+      <TextInput
+        Icon={EnvelopeIcon}
+        inputProps={{
+          type: "text",
+          placeholder: "Enter Email",
+          value: invitedMemberEmail,
+          onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+            setInvitedMemberEmail(e.target.value),
+        }}
+      />
+      <button
+        className='-mt-5 flex h-16 w-full items-center justify-center rounded-md bg-white px-3 py-1.5 font-subtext text-2xl leading-6 text-blue-imperial shadow-sm transition-colors hover:bg-white/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-royal disabled:bg-white/50'
+        onClick={() => {
+          inviteTeamMember(user, invitedMemberEmail)
+            .then(team => {
+              props.setTeamPage(team)
+              setInvitedMemberEmail("")
+              toast.success("Team Member Invited!")
+            })
+            .catch(error => {
+              toast.error(error.message)
+            })
+        }}
+      >
+        INVITE MEMBER
+      </button>
+    </div>
+  )
 }
