064b74a81203adf5254690d22181595d67c5df82
Zack Traczyk <me@zacktraczyk.com>
2024-01-16 23:28:57 -0800

    timeout decrease 100 -> 50

--- src/views/portal/admin/QRCheckIn/index.tsx
+++ src/views/portal/admin/QRCheckIn/index.tsx
@@ -17,7 +17,7 @@ const QRCheckInContainer: React.FC = () => {
       console.log(result)
       handleScanUID(result.getText())
     },
-    timeBetweenDecodingAttempts: 100,
+    timeBetweenDecodingAttempts: 50,
     onError: error => {
       console.error(error)
     },
